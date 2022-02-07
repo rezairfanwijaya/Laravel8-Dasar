0d27f4b631b615bd9d617e3829a23aa32912b047
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-02-07 23:04:34 +0700

    menginstall bootstrap menggunakan laravel UI

--- MyApp/webpack.mix.js
+++ MyApp/webpack.mix.js
@@ -1,19 +1,16 @@
 const mix = require('laravel-mix');
 
-// mixing script js
-mix.scripts([
-    'resources/js/my-script.js',
-    'resources/js/my-script-console.js',
-], 'public/js/hasil-mix.js').version();
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
 
-// mixing style
-mix.styles([
-    'resources/css/my-style-h1.css',
-    'resources/css/my-style.css',
-], 'public/css/hasil-mix.css').version()
-
-// mixing sass
-mix.sass('resources/sass/my-sass.scss', 'public/sass/sass.css').version()
-
-// penambahan .version() diatas untuk berujuan membuat version dari file hasil-mix.js, hasil-mix.scc dan sass.css untuk menyimpan cache pada browser supaya tidak mengunduh lagi dari server
-// agar version ini bisa berjalan maka pada view saat pemanggilan asset css/js jangan menggunakan asser() tapi ganti menggunakan mix()
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
