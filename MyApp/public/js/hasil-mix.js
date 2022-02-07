dbbeb36b5fe9a80b76d0dcee742c09e2bc915489
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-02-07 22:19:10 +0700

    melakukan minified file mengunakan laravel dengan perintah run dev prodatau bisa juga dengan perintah npm run production

--- MyApp/public/js/hasil-mix.js
+++ MyApp/public/js/hasil-mix.js
@@ -1,9 +1,1 @@
-// tampilkan alert ketika halaman di klik
-window.addEventListener('click', function() {
-    this.alert('saya di klik')
-})
-
-// tampilakn info ke console ketika halaman di klik
-window.addEventListener('click', function() {
-    console.info('saya juga diklik')
-})
+window.addEventListener("click",(function(){this.alert("saya di klik")})),window.addEventListener("click",(function(){console.info("saya juga diklik")}));
