cc758b7bef105ee5f9e2aaf98bfda2caa98a80ce
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-02-15 22:15:46 +0700

    migration (belum selesai) tinggal lanjutin yang alter table halaman 337

--- MyApp/app/Models/mahasiswa.php
+++ MyApp/app/Models/mahasiswa.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Laptop extends Model
+class mahasiswa extends Model
 {
     use HasFactory;
-    public function nama(){
-        return "Kalimat ini diambil dari external class";
-    }
 }
---
+++ MyApp/database/migrations/2022_02_15_145852_create_mahasiswas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->char('nim',8)->unique();
+            $table->string('nama');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('fakultas');
+            $table->string('jurusan');
+            $table->decimal('ipk',3,2)->default(1.00);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
