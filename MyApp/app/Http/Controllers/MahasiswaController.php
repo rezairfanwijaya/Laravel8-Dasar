52dfd936b4c70e43d0e234a21f94ff10b88a7f65
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-02-21 01:20:22 +0700

    membuat dokumentasi dbfacade, modul tinggal db::Statement halaman 356

--- MyApp/app/Http/Controllers/MahasiswaController.php
+++ MyApp/app/Http/Controllers/MahasiswaController.php
@@ -9,21 +9,32 @@ use Illuminate\Support\Facades\DB;
 class MahasiswaController extends Controller
 {
     public function dbFacade (){
-        return view('learning.dbFacade')->with('title', 'DB Facade');
+        $show = DB::select("SELECT * FROM mahasiswas WHERE nama = ?", ['abdas']);
+        $showprepared = DB::select("SELECT * FROM mahasiswas WHERE nama = ?", ['Reza Adas']);
+        $showparameter = DB::select("SELECT * FROM mahasiswas WHERE nama = ?", ['ipang']);
+        return view('learning.dbFacade')
+        ->with('title', 'DB Facade')
+        ->with('data',$show)
+        ->with('prepared',$showprepared)
+        ->with('parameter',$showparameter)
+        ;
     }
 
     public function insertSql(){
         $insert = DB::insert("INSERT INTO mahasiswas(nim,nama,tanggal_lahir) VALUES (88675388, 'abdas', '2000-11-13')");
-    \var_dump($insert);
+    // \var_dump($insert);  
+
+        return redirect ('/facade');
     }
 
     public function insertPrepared(){
         $insert = DB::insert('
         INSERT INTO mahasiswas (nim,nama,tanggal_lahir,ipk,created_at,updated_at)
-        VALUES(?,?,?,?,?,?)', [19102149, ' Reza Adas', '2000-11-13', 4.00, now(), now()]
+        VALUES(?,?,?,?,?,?)', [19102149, 'Reza Adas', '2000-11-13', 4.00, now(), now()]
         );
+        // var_dump($insert);
 
-        var_dump($insert);
+        return redirect ('/facade');
     }
 
     public function insertBinding(){
@@ -36,7 +47,9 @@ class MahasiswaController extends Controller
             'updated' => now()
             
         ]);
-        var_dump($result);
+        // var_dump($result);
+
+        return redirect ('/facade');
     }
 
     public function update(){
@@ -44,8 +57,14 @@ class MahasiswaController extends Controller
         var_dump($update);
     }
 
-    public function delete(){
-        $delete = DB::delete("DELETE FROM mahasiswas WHERE id = ?", [4]);
+    public function delete($id){
+        $delete = DB::delete("DELETE FROM mahasiswas WHERE id = ?", [$id]);
         var_dump($delete);
+        return redirect ('/facade');
+    }
+
+    public function show (){
+        $show = DB::select("SELECT * FROM mahasiswas");
+@dd($show);
     }
 }
--- MyApp/resources/views/learning/dbFacade.blade.php
+++ MyApp/resources/views/learning/dbFacade.blade.php
@@ -1,4 +1,79 @@
 @extends('template.index')
 @section('konten')
-    facade
+    <div class="konten mt-3 bg-white rounded shadow-sm p-3">
+        <ol>
+            <div class="insert">
+                <li>Insert Data</li>
+                <div class="list">
+                    <ul>
+                        <li class="mt-3">Insert Biasa</li>
+                        <code>$insert = DB::insert("INSERT INTO mahasiswas(nim,nama,tanggal_lahir) VALUES (88675388,
+                            'abdas', '2000-11-13')");</code>
+                        <br>
+                        {{-- proses insert data --}}
+                        <a href="{{ url('./insert-sql') }}">Klik disini</a> untuk menjalankan code
+
+                        {{-- menampilkan hasil insert biasa --}}
+                        @forelse ($data as $item)
+                            <div class="data">
+                                id : {{ $item->id }}, nama : {{ $item->nama }}, nim : {{ $item->nim }}, tanggal
+                                lahir : {{ $item->tanggal_lahir }}
+                                <a href="{{ route('delete', ['id'=>$item->id] )}}">(delete)</a>
+                            </div>
+                        @empty
+                        @endforelse
+
+                        <li class="mt-3">Insert menggunakan prepared statement</li>
+                        <code> $insert = DB::insert('
+                            INSERT INTO mahasiswas (nim,nama,tanggal_lahir,ipk,created_at,updated_at)
+                            VALUES(?,?,?,?,?,?)', [19102149, ' Reza Adas', '2000-11-13', 4.00, now(), now()]
+                            );</code>
+                        <br>
+                        {{-- proses insert data dengan prepared statement --}}
+                        <a href="{{ url('/insert-prepared') }}">Klik disini</a> untuk menjalankan code
+
+                        {{-- hasil insert prepared --}}
+                        @forelse ($prepared as $item)
+                            <div class="data">
+                                id : {{ $item->id }}, nama : {{ $item->nama }}, nim : {{ $item->nim }}, tanggal
+                                lahir : {{ $item->tanggal_lahir }}
+                                <a href="{{ route('delete', ['id'=>$item->id] )}}">(delete)</a>
+                            </div>
+                        @empty
+                        @endforelse
+
+                        <li class="mt-3">Insert menggunakan named parameter</li>
+                        <code>  $result = DB::insert('INSERT INTO mahasiswas (nim,nama,tanggal_lahir,ipk,created_at,updated_at) VALUES (:nim, :nama, :tl, :ipk, :created, :updated)', [
+                            'nim' => '19102146',
+                            'nama' => 'ipang',
+                            'tl' => '2000-11-13',
+                            'ipk' => '3.56',
+                            'created' => now(),
+                            'updated' => now()
+                            
+                        ]);</code>
+                        <br>
+                        {{-- proses insert data dengan prepared statement --}}
+                        <a href="{{ url('/insert-binding') }}">Klik disini</a> untuk menjalankan code
+
+                        {{-- hasil insert prepared --}}
+                        @forelse ($parameter as $item)
+                            <div class="data">
+                                id : {{ $item->id }}, nama : {{ $item->nama }}, nim : {{ $item->nim }}, tanggal
+                                lahir : {{ $item->tanggal_lahir }} 
+                                <a href="{{ route('delete', ['id'=>$item->id] )}}">(delete)</a>
+                            </div>
+                            
+                        @empty
+                        @endforelse
+                    </ul>
+
+                </div>
+            </div>
+
+            <div class="update mt-4">
+                <li>Update Data</li>
+            </div>
+        </ol>
+    </div>
 @endsection
